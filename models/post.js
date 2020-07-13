b15b13acc55fc726e3b79f1f371b3da1edc7ad58
DamianArado <khanhumayun95@gmail.com>
2020-07-13 17:45:16 +0000

    Add model files

---
+++ models/post.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+	title: String,
+	price: String,
+	description: String,
+	images: [ { url: String, public_id: String } ],
+	location: String,
+	geometry: {
+		type: {
+			type: String,
+			enum: ['Point'],
+			required: true
+		},
+		coordinates: {
+			type: [Number],
+			required: true
+		}
+	},
+	properties: {
+		description: String
+	},
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	reviews: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Review'
+		}
+	],
+	avgRating: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model('Posts',PostSchema);
